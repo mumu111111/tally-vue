781cab151e071a1027f6557c6734a23885c8372d
mumu111111 <2646709035@qq.com>
2021-04-19 15:20:22 +0800

    numberpad部分组件完成

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>OK</button>
-      <button>0</button>
-      <button>.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="remove">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="clear">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent">.</button>
     </div>
   </div>
 </template>
@@ -25,11 +25,49 @@ import Vue from "vue";
 import { Component } from "vue-property-decorator";
 
 @Component
-export default class NumberPad extends Vue {}
+export default class NumberPad extends Vue {
+  output: string = "0";
+
+  inputContent(event: MouseEvent) {
+    const Button = event.target as HTMLButtonElement;
+    const input = Button.textContent!;
+    if (Button === null) return; // 加判断这句 否则报错
+    if (this.output.length === 16) {
+      return;
+    }
+    if (this.output === "0") {
+      if ("0123456789".indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf(".") >= 0 && input === ".") {
+      return;
+    }
+    this.output += input;
+  }
+
+  remove() {
+    if (this.output.length === 1) {
+      this.output = "0";
+    } else {
+      this.output = this.output.slice(0, -1);
+    }
+  }
+
+  clear() {
+    this.output = "0";
+  }
+
+  // ok() {}
+}
 </script>
 
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
+
 .numberPad {
   > .output {
     @extend %clearFix;
@@ -38,16 +76,19 @@ export default class NumberPad extends Vue {}
     font-family: Consolas, monospace;
     padding: 9px 16px;
     text-align: right;
+    height: 72px;
   }
 
   > .buttons {
     @extend %clearFix;
+
     > button {
       float: left;
       width: 25%;
       height: 64px;
       background: transparent;
       border: none;
+
       &.ok {
         float: right;
         height: 64 * 2px;
@@ -56,32 +97,40 @@ export default class NumberPad extends Vue {}
       &.zero {
         width: 25 * 2%;
       }
+
       $bg: #f2f2f2;
+
       &:nth-child(1) {
         background: $bg;
       }
+
       &:nth-child(2),
       &:nth-child(5) {
         background: darken($bg, 4%);
       }
+
       &:nth-child(3),
       &:nth-child(6),
       &:nth-child(9) {
         background: darken($bg, 4 * 2%);
       }
+
       &:nth-child(4),
       &:nth-child(7),
       &:nth-child(10) {
         background: darken($bg, 4 * 3%);
       }
+
       &:nth-child(8),
       &:nth-child(11),
       &:nth-child(13) {
         background: darken($bg, 4 * 4%);
       }
+
       &:nth-child(14) {
         background: darken($bg, 4 * 5%);
       }
+
       &:nth-child(12) {
         background: darken($bg, 4 * 6%);
       }
