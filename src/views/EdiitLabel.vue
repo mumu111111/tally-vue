e2391966956f738cd5ed8a0ec15048c7a36b8727
mumu111111 <2646709035@qq.com>
2021-04-20 08:11:00 +0800

    新增编辑标签页

--- src/views/EdiitLabel.vue
+++ src/views/EdiitLabel.vue
@@ -5,9 +5,24 @@
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
+import { tagListModel } from "@/models/tagListModel";
 
 @Component
-export default class EditLabel extends Vue {}
+export default class EditLabel extends Vue {
+  created() {
+    // 获取router id
+    const id = this.$route.params.id;
+    tagListModel.fetch(); //获取data
+    // 判断tag 是否存在
+    const tags = tagListModel.data;
+    const tag = tags.filter((t) => (t.id = id))[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
