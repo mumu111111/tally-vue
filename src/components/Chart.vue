81c40ec6e9c8a7de67082f9db34ba75c87f233ff
mumu111111 <2646709035@qq.com>
2021-04-20 15:16:45 +0800

    echart图表

---
+++ src/components/Chart.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="wrapper" ref="wrapper">jjj</div>
+</template>
+<script lang="ts">
+import Vue from "vue";
+import { Component, Watch, Prop } from "vue-property-decorator";
+import echarts, { EChartsOption } from "echarts";
+
+@Component
+export default class Chart extends Vue {
+  @Prop() options?: EChartsOption;
+
+  mounted() {
+    if (!this.options) {
+      return console.error("options为空");
+    }
+    // const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    const chart = echarts.init(this.$refs.wrapper as HTMLDivElement);
+    // 使用刚指定的配置项和数据显示图表。
+    chart.setOption(this.options);
+  }
+}
+</script>
+<style lang="scss" scoped>
+.wrapper {
+  height: 400px;
+}
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,6 +3,7 @@
     <Layout>
       <h3>支出</h3>
       <Tabs :data-source="intervalList" :value.sync="interval" />
+      <Chart :options="x" />
     </Layout>
   </div>
 </template>
@@ -11,9 +12,10 @@
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import Tabs from "@/components/Tabs.vue";
+import Chart from "@/components/Chart.vue";
 
 @Component({
-  components: { Tabs },
+  components: { Tabs, Chart },
 })
 export default class Statistics extends Vue {
   type = "-";
@@ -23,6 +25,110 @@ export default class Statistics extends Vue {
     { text: "按周", value: "week" },
     { text: "按月", value: "month" },
   ];
+  get x() {
+    return {
+      grid: {
+        left: 10,
+        right: 10,
+      },
+      xAxis: {
+        type: "category",
+        data: [
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+          "10",
+          "11",
+          "12",
+          "13",
+          "14",
+          "15",
+          "16",
+          "17",
+          "18",
+          "19",
+          "20",
+          "21",
+          "22",
+          "23",
+          "24",
+          "25",
+          "26",
+          "27",
+          "28",
+          "29",
+          "30",
+        ],
+        axisLabel: {
+          interval: 3,
+          lineStyle: { width: 1 },
+          axisTick: { show: false },
+        },
+        axisTick: { show: false },
+        axisLine: { lineStyle: { color: "#D1D1D1" } },
+      },
+      yAxis: {
+        type: "value",
+        show: false,
+      },
+
+      series: [
+        {
+          symbol: "circle",
+          symbolSize: 10,
+          itemStyle: {
+            borderWidth: 1,
+            color: "#666",
+            shadowColor: "yellow",
+          },
+          data: [
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            820,
+            932,
+            932,
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            820,
+            932,
+            1330,
+            820,
+            932,
+            901,
+            934,
+            1290,
+            1330,
+            1320,
+            214,
+            421,
+            533,
+          ],
+          type: "line",
+        },
+      ],
+      tooltip: {
+        show: true,
+        formatter: "{c}",
+        position: "top",
+      },
+    };
+  }
 }
 </script>
 
